4c2f442798bcca08cc4dfdbfcf83c6f8770799b6
lvalenty <lvalenty@gmail.com>
2013-05-19 11:22:39 -0700

    updated version

--- gen/com/lukevalenty/rpgforge/R.java
+++ gen/com/lukevalenty/rpgforge/R.java
@@ -8,6 +8,12 @@
 package com.lukevalenty.rpgforge;
 
 public final class R {
+    public static final class anim {
+        public static final int left_slide_in=0x7f040000;
+        public static final int left_slide_out=0x7f040001;
+        public static final int right_slide_in=0x7f040002;
+        public static final int right_slide_out=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,38 +23,72 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int decor_shadow=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_delete=0x7f020000;
+        public static final int ic_action_draw=0x7f020001;
+        public static final int ic_action_eyedrop=0x7f020002;
+        public static final int ic_action_fill=0x7f020003;
+        public static final int ic_action_lock=0x7f020004;
+        public static final int ic_action_move=0x7f020005;
+        public static final int ic_action_new=0x7f020006;
+        public static final int ic_action_playtest=0x7f020007;
+        public static final int ic_action_redo=0x7f020008;
+        public static final int ic_action_save=0x7f020009;
+        public static final int ic_action_undo=0x7f02000a;
+        public static final int ic_action_unlock=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int gridView1=0x7f080003;
-        public static final int mapView=0x7f080004;
-        public static final int menu_draw=0x7f080008;
-        public static final int menu_move=0x7f080007;
-        public static final int menu_redo=0x7f080006;
-        public static final int menu_undo=0x7f080005;
-        public static final int pager=0x7f080000;
-        public static final int pager_title_strip=0x7f080001;
-        public static final int rpg_list=0x7f080002;
+        public static final int gameView=0x7f090004;
+        public static final int gridView1=0x7f09000c;
+        public static final int heightField=0x7f09000f;
+        public static final int mapNameField=0x7f09000d;
+        public static final int mapView=0x7f09000b;
+        public static final int menu_deletemap=0x7f090019;
+        public static final int menu_draw=0x7f090015;
+        public static final int menu_eyedrop=0x7f090014;
+        public static final int menu_fill=0x7f090016;
+        public static final int menu_move=0x7f090013;
+        public static final int menu_new=0x7f090011;
+        public static final int menu_playtest=0x7f090012;
+        public static final int menu_renamemap=0x7f090017;
+        public static final int menu_resizemap=0x7f090018;
+        public static final int onScreenDPad=0x7f090005;
+        public static final int pager=0x7f090000;
+        public static final int pager_title_strip=0x7f090001;
+        public static final int projectName=0x7f090010;
+        public static final int resizeHeightField=0x7f090009;
+        public static final int resizeWidthField=0x7f090007;
+        public static final int rpgListView=0x7f090002;
+        public static final int rpg_list=0x7f090003;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090008;
+        public static final int textView3=0x7f09000a;
+        public static final int widthField=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
-        public static final int mapedit=0x7f030002;
+        public static final int browse_activity=0x7f030001;
+        public static final int fragment_main_dummy=0x7f030002;
+        public static final int gameview=0x7f030003;
+        public static final int list_item=0x7f030004;
+        public static final int map_info=0x7f030005;
+        public static final int mapedit=0x7f030006;
+        public static final int new_map_dialog=0x7f030007;
+        public static final int new_project_dialog=0x7f030008;
+        public static final int tiledrawer=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int title_section1=0x7f060002;
+        public static final int title_section2=0x7f060003;
     }
     public static final class style {
         /** 
@@ -72,10 +112,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
